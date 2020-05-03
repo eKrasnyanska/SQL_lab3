008419ad357724ac185cad4d00deb8659f077451
Yelyzaveta Krasnyanskaya <lizahtconex@gmail.com>
2020-05-03 13:55:23 +0300

    new version view.sql

--- view.sql
+++ view.sql
@@ -1,15 +1,13 @@
-CREATE OR REPLACE VIEW FILMS_REVIEW AS 
-  select c.company_name as company, 
-f.film_name as film, 
-f.income as income,
-g.genre_name as genre,
-to_number(to_char(f.release_date, 'YYYY')) as release_date
-from company c 
-    join company_film cf
-    on c.company_name = cf.company_company_name
-    join film f 
-    on cf.film_film_name = f.film_name
-    join film_genre fg
-    on f.film_name = fg.film_film_name
-    join genre g
-    on fg.genre_genre_name = g.genre_name;
+CREATE VIEW films_review AS
+SELECT
+company_film.company_company_name as company,
+film.film_name as film,
+film.income as income,
+film_genre.genre_genre_name as genre,
+to_number(to_char(film.release_date, 'YYYY')) as release_date
+FROM company_film
+    JOIN film 
+    ON company_film.film_film_name = film.film_name AND company_film.film_director = film.director
+    JOIN film_genre
+    ON film.film_name = film_genre.film_film_name AND film.director = film_genre.film_director
+;
